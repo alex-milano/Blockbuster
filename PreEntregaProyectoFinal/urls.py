8a8d7bd007d99a5424f4ae9a89943e9a9b768d29
Kolektor <jose.piconrisso@cba.gov.ar>
2022-01-19 19:09:02 -0300

    Se agrega template base y se configuran las views con los templates correspondientes

--- PreEntregaProyectoFinal/urls.py
+++ PreEntregaProyectoFinal/urls.py
@@ -1,14 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from Videoclub.views import index
 from Videoclub import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Videoclub/',include('Videoclub.urls')),
-    path('listadoPeliculas/', views.listado_peliculas),
-    path('ConfirmacionAlta/', views.alta_pelicula)
+    # path('listadoPeliculas/', views.listado_peliculas),
+    # path('ConfirmacionAlta/', views.alta_pelicula)
     
-   
 ]
