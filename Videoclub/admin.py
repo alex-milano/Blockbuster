e0532bc16e757034824ef98a7897bdd90be3f1d2
alex-milano <alexjmilano@gmail.com>
2022-02-15 22:21:55 -0300

    lastdance

--- Videoclub/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import * 
-
-# Register your models here.
-
-admin.site.register(Pelicula)
-admin.site.register(Socio)
-admin.site.register(Empleado)
-admin.site.register(Avatar)
--- Videoclub/forms.py
+++
@@ -1,9 +0,0 @@
-from urllib import request
-from django import forms
-
-class Formulario_peliculas(forms.Form):
-    nombre = forms.CharField(max_length= 100)
-    categoria = forms.CharField(max_length= 100)
-    descripcion = forms.CharField(max_length=100)
-    estado = forms.BooleanField()
-    precio = forms.IntegerField()
